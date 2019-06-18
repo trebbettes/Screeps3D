0633d56a6e59227964d964adbee3f35e325d31ed
thmsn <thmsn.dk@gmail.com>
2019-06-19 01:31:29 +0200

    #6 Prevent tombstone "teleporting" when spawning, spawn in place instead

--- Assets/Scripts/Screeps3D/RoomObjects/Views/TombstoneView.cs
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/TombstoneView.cs
@@ -26,19 +26,19 @@ namespace Screeps3D.RoomObjects.Views
         {
             base.Delta(data);
 
-            var posDelta = _posTarget - RoomObject.Position;
+            //var posDelta = _posTarget - RoomObject.Position;
 
-            if (posDelta.sqrMagnitude > .01)
-            {
-                _posTarget = RoomObject.Position;
-            } 
+            //if (posDelta.sqrMagnitude > .01)
+            //{
+            //    _posTarget = RoomObject.Position;
+            //} 
         }
 
         private void Update()
         {
             if (_tombstone == null)
                 return;
-            transform.localPosition = Vector3.SmoothDamp(transform.localPosition, _posTarget, ref _posRef, .5f);
+            //transform.localPosition = Vector3.SmoothDamp(transform.localPosition, _posTarget, ref _posRef, .5f);
             //_rotationRoot.transform.rotation = Quaternion.Slerp(_rotationRoot.transform.rotation, _tombstone.Rotation, Time.deltaTime * 5);
         }
     }
