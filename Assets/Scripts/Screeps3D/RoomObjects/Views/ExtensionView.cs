5551b878b2300530910110c2f0d1740c4672509c
Robalian <44264597+Robalian@users.noreply.github.com>
2019-06-20 01:34:43 +0200

    added extension scaling based on RCL (#14)
    fixes #5

---
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/ExtensionView.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using UnityEngine;
+
+namespace Screeps3D.RoomObjects.Views
+{
+    public class ExtensionView: MonoBehaviour, IObjectViewComponent
+    {
+        [SerializeField] private ScaleAxes _size;
+        private Extension _extension;
+
+        public void Init()
+        {
+        }
+
+        public void Load(RoomObject roomObject)
+        {
+            _extension = roomObject as Extension;
+        }
+
+        public void Delta(JSONObject data)
+        {
+            if (_extension.EnergyCapacity >= 200)
+                _size.SetVisibility(0.85f);
+            else if (_extension.EnergyCapacity >= 100)
+                _size.SetVisibility(0.65f);
+            else
+                _size.SetVisibility(0.5f);
+        }
+
+        public void Unload(RoomObject roomObject)
+        {
+            _extension = null;
+        }
+    }
+}
