98c000ecddb8d30112b66fbdf26258da42fd8351
thmsn <thmsn.dk@gmail.com>
2019-06-27 00:26:08 +0200

    Scaling of size/visibility for source based on energy left

---
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/SourceView.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+using UnityEngine;
+
+namespace Screeps3D.RoomObjects.Views
+{
+    public class SourceView : MonoBehaviour, IObjectViewComponent
+    {
+        //[SerializeField] private ScaleAxes _energyDisplay;
+        [SerializeField] private ScaleVisibility _Visibility;
+        private Source _source;
+
+        public void Init()
+        {
+        }
+
+        public void Load(RoomObject roomObject)
+        {
+            _source = roomObject as Source;
+        }
+
+        public void Delta(JSONObject data)
+        {
+            var visibility = _source.Energy / _source.EnergyCapacity;
+            _Visibility.SetVisibility(visibility);
+        }
+
+        public void Unload(RoomObject roomObject)
+        {
+        }
+    }
+}
