0d0a66540f7925df3bfc3f67f8b88e910b790259
thmsndk <thmsn.dk@gmail.com>
2019-07-15 20:25:15 +0200

    Feature/#21 initial compass (#59)
    Closes #21 
    
    * Added initial hud compass rendered with a PNG because SVG was giving me trouble
    
    * unity was not saved properly, fixed object hierachy
    
    * More unsaved changes, compass renders and rotates now.
    
    * Attempt at masking start and end with sprite mask, did not appear to work, also cleaned up components on compass.
    
    * Added a gradient Soft Mask on edges for the compass #21
    
    * Added roomname below compass

---
+++ Assets/Scripts/Screeps3D/CompassHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+using TMPro;
+using Screeps3D.Player;
+
+public class CompassHandler : MonoBehaviour
+{
+    public float numberOfPixelsNorthToNorth;
+    //public GameObject target;
+    Vector3 startPosition;
+    float rationAngleToPixel;
+    public RawImage compass;
+    //public SVGImage compass;
+    public Transform player;
+
+    public TMP_Text roomName;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        rationAngleToPixel = numberOfPixelsNorthToNorth / 360f;
+    }
+
+    void Update()
+    {
+        //Vector3 perp = Vector3.Cross(Vector3.forward, player.transform.forward);
+        //float dir = Vector3.Dot(perp, Vector3.up);
+
+        //var plane2d = Vector3.Angle(player.transform.forward, Vector3.forward);
+        //var plane3d = Vector3.Angle(new Vector3(player.transform.forward.x, 0f, player.transform.forward.z), Vector3.forward);
+
+        //compass.transform.position = startPosition + (new Vector3(plane2d * Mathf.Sign(dir) * rationAngleToPixel, 0, 0));
+
+        //compass.transform.position = startPosition + new Vector3(1f, 0f, 0f); //+ new Vector3(player.localEulerAngles.y / 360f, 0);
+
+        // RawImage
+        compass.uvRect = new Rect(player.localEulerAngles.y / 360f, 0, 1, 1);
+
+        if (roomName != null)
+        {
+            roomName.text = PlayerPosition.Instance.RoomName;
+        }
+    }
+}
