00a2d7f30b38bb33538f856f89b76a6731fd530d
thmsn <thmsn.dk@gmail.com>
2019-07-19 18:39:09 +0200

    Scale creep sizes based on amount of body parts

--- Assets/Scripts/Screeps3D/RoomObjects/Views/CreepView.cs
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/CreepView.cs
@@ -20,6 +20,28 @@ namespace Screeps3D.RoomObjects.Views
 
             _rotTarget = transform.rotation;
             _posTarget = roomObject.Position;
+
+            ScaleCreepSize();
+        }
+
+        private void ScaleCreepSize()
+        {
+            var percentage = _creep.Body.Parts.Count / 50f;
+
+            var minVisibility = 0.001f; /*to keep it visible and selectable*/
+            var maxVisibility = 1f;
+
+            // http://james-ramsden.com/map-a-value-from-one-number-scale-to-another-formula-and-c-code/
+            float minimum = Mathf.Log(minVisibility);
+            float maximum = Mathf.Log(maxVisibility);
+
+            // Scale the visibility in such a way that a lot of the model is rendered above 25 body parts
+            float current = Mathf.Log(percentage == 0 ? minVisibility : percentage);
+
+            // Map range to visibility range
+            var visibility = minVisibility + (maxVisibility - minVisibility) * ((current - minimum) / (maximum - minimum));
+
+            _vis.SetVisibility(visibility, true);
         }
 
         internal override void Delta(JSONObject data)
