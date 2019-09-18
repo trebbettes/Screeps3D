0b2cbd15fcd55cd1e6bd288a9afc121fcb686cbc
thmsn <thmsn.dk@gmail.com>
2019-09-18 20:07:31 +0200

    Disabled rendering of RMA as a beam effect #8

--- Assets/Scripts/Screeps3D/RoomObjects/Views/CreepBeamView.cs
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/CreepBeamView.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Assets.Scripts.Screeps_API.ConsoleClientAbuse;
 using Screeps3D.Effects;
 using UnityEngine;
 
@@ -10,7 +11,7 @@ namespace Screeps3D.RoomObjects.Views
         private static readonly Dictionary<string, BeamConfig> BeamConfigs = new Dictionary<string, BeamConfig>
         {   
             {"rangedAttack", new BeamConfig(Color.blue, 0.3f, 0.3f)},
-            {"rangedMassAttack", new BeamConfig(Color.blue, 0.3f, 0.3f)},
+            //{"rangedMassAttack", new BeamConfig(Color.blue, 0.3f, 0.3f)}, // RMA is an AOE effect, not a beam.
             {"rangedHeal", new BeamConfig(Color.green, 0.3f, 0.3f)},
             {"repair", new BeamConfig(Color.yellow, 0.3f, 0.3f)},
             {"build", new BeamConfig(Color.yellow, 0.3f, 0.3f)},
@@ -32,7 +33,8 @@ namespace Screeps3D.RoomObjects.Views
         {
             var beam = BeamConfigs.FirstOrDefault(c => _creep.Actions.ContainsKey(c.Key) && !_creep.Actions[c.Key].IsNull);
             if (beam.Value == null) return;
-            EffectsUtility.Beam(_creep as RoomObject, _creep.Actions[beam.Key], beam.Value);
+            var action = _creep.Actions[beam.Key];
+            EffectsUtility.Beam(_creep as RoomObject, action, beam.Value);
             // StartCoroutine(Beam.Draw(_creep, _creep.Actions[beam.Key], _lineRenderer, beam.Value));
             
         }
